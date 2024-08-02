8f8d87d9bf4e5433bfb079ca682bb009151035c1
GabrielT23 <gabaraujoteixeira@gmail.com>
2024-08-02 10:07:36 -0300

    feat:configuring-prisma-client-creating-connection-with-bank-and-creating-users-table

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from '@modules/prisma/infra/database/prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
