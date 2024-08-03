7462c9188c41f5fc100fe28464899e2d2db85f95
Jhonatas J. Mendonça <118845977+Jhonatas56@users.noreply.github.com>
2024-08-03 14:56:51 -0300

    feat: add multer module in core module

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -2,6 +2,9 @@ import { Global, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './data/prisma/prisma.service';
 import { configuration } from './infra/config/configuration';
+import { MulterModule } from '@nestjs/platform-express';
+import { StorageProvider } from './data/storage';
+import { FileService } from './infra/services/file.service';
 
 @Global()
 @Module({
@@ -10,8 +13,11 @@ import { configuration } from './infra/config/configuration';
       load: [configuration],
       isGlobal: true,
     }),
+    MulterModule.register({
+      dest: '../../tmp',
+    }),
   ],
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [PrismaService, StorageProvider, FileService],
+  exports: [PrismaService, StorageProvider, FileService],
 })
 export class CoreModule {}
