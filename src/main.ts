22516f99812faf1170ba491a7b101eb28f63744a
João Vitor Aguiar <121316279+JoaoVitorAguiar@users.noreply.github.com>
2024-08-04 02:03:37 -0300

    feat: add front end url in cors

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,16 @@ import { configuration } from '@core/infra/config/configuration';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = configuration();
-  app.enableCors();
+  app.enableCors({
+    origin: config.frontEndUrl,
+    allowedHeaders: ['content-type', 'authorization'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
